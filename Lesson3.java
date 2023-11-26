d91644af884f8ff0b93b41b3254e089d1a617261
yogev31 <99199888+yogev31@users.noreply.github.com>
2023-11-26 14:37:16 +0200

    Update Lesson3.java

--- Lesson3.java
+++ Lesson3.java
@@ -1,7 +1,10 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        sumNums(375);
 
     }
     public static void avg(int a, int b, int c)
@@ -17,6 +20,57 @@ public class Main {
     {
         int y = 2*x+5;
         System.out.println("Y = " + y);
+    }
+
+    public static void is(int x)
+    {
+        if(x > 0)
+        {
+            System.out.println("Positive");
+        }
+        else if (x < 0)
+        {
+            System.out.println("negative");
+        }
+    }
+    public static void fre(int x)
+    {
+        double price = 0;
+        if(x > 108)
+        {
+            System.out.println("Cant buy more than 108!");
+        }
+        else
+        {
+            price += (x/54)*50;
+            x = x%54;
+            price += (x/10)*10;
+            x = x%10;
+            price += x*1.1;
+            System.out.println("price = " + price);
+        }
+    }
+    public static void id(Scanner scan)
+    {
+        System.out.println("Enter your full name: ");
+        String name = scan.next();
+        System.out.println("Enter your school's name: ");
+        String school = scan.next();
+        System.out.println("Enter your amount of subjects: ");
+        int num = scan.nextInt();
+        System.out.println("My name is " + name + ", My school is " + school + ", The num o subjects " + num);
+    }
+
+    public static void sumNums(int a)
+    {
+        int num = 0;
+        num += a % 10;
+        a = a / 10;
+        num += a % 10;
+        a = a / 10;
+        num += a;
+        System.out.println("num = " + num);
+
     }
 
 
