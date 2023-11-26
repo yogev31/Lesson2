c5d25209e5e599834946fa3faa9e052333044196
yogev31 <99199888+yogev31@users.noreply.github.com>
2023-11-26 14:04:06 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,23 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+    }
+    public static void avg(int a, int b, int c)
+    {
+        System.out.println("Avg is: " + (a+b+c)/3.0);
+    }
+    public static void yeter(int a, int b)
+    {
+        System.out.println("yeter is: " + Math.sqrt((Math.pow(a,2)+Math.pow(b,2))));
+    }
+
+    public static void func(int x)
+    {
+        int y = 2*x+5;
+        System.out.println("Y = " + y);
+    }
+
+
+}
